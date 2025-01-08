1047eb7d29907a912ba1cdc8377363ea61a98d57
Deservate <mrclonetrooper2@gmail.com>
2025-01-08 22:47:10 +0100

    dec2

---
+++ 2019/dec2.py
@@ -0,0 +1,26 @@
+with open('2019/input/dec2_input.txt', 'r') as file:
+    input = [int(num) for num in file.read().split(',')]
+
+def calculate_output(input, noun, verb):
+    mod_input = [input[0]] + [noun] + [verb] + input[3:]
+    i = 0
+    while True:
+        match mod_input[i]:
+            case 1:
+                mod_input[mod_input[i + 3]] = mod_input[mod_input[i + 1]] + mod_input[mod_input[i + 2]]
+            case 2:
+                mod_input[mod_input[i + 3]] = mod_input[mod_input[i + 1]] * mod_input[mod_input[i + 2]]
+            case 99:
+                break
+        i += 4
+    return mod_input[0]
+
+# part 1
+print(calculate_output(input, 12, 2))
+
+# part 2
+for noun in range(100):
+    for verb in range(100):
+        if calculate_output(input, noun, verb) == 19690720:
+            print(100 * noun + verb)
+            exit()
