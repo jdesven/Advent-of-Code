77b4407f3e9bb4490254251a65f11fd2aedd9f15
Deservate <mrclonetrooper2@gmail.com>
2025-01-09 17:39:39 +0100

    dec3

---
+++ 2019/dec3.py
@@ -0,0 +1,36 @@
+with open('2019/input/dec3_input.txt', 'r') as file:
+    wires = [line.split(',') for line in file.read().splitlines()]
+
+def calculate_path(wire):
+    path = {}
+    location = 0j
+    location_i = 0
+    for instruction in wire:
+        match instruction[0]:
+            case 'L':
+                dir = -1 + 0j
+            case 'U':
+                dir = -1j
+            case 'R':
+                dir = 1 + 0j
+            case 'D':
+                dir = 1j
+        for i in range(1, int(instruction[1:]) + 1):
+            location_i += 1
+            if location + i * dir not in path:
+                path[location + i * dir] = location_i
+        location = location + int(instruction[1:]) * dir
+    return path
+
+crossing_points = set()
+path_1 = calculate_path(wires[0])
+path_2 = calculate_path(wires[1])
+for location, i_location in path_1.items():
+    if location in path_2:
+        crossing_points.add((location, i_location, path_2[location]))
+
+distances_to_center = [abs(int(crossing_point[0].real)) + abs(int(crossing_point[0].imag)) for crossing_point in crossing_points]
+print(min(distances_to_center))
+
+distances_travelled = [crossing_point[1] + crossing_point[2] for crossing_point in crossing_points]
+print(min(distances_travelled))
