5ad13199f4e2699e5fd316efda37d1176bc57a24
jdesven <mrclonetrooper2@gmail.com>
2025-01-15 13:53:46 +0100

    dec2 turned while loop into for loop to make it more predictable, removed old dec4 file

--- 2019/dec2.py
+++ 2019/dec2.py
@@ -3,17 +3,14 @@ with open('2019/input/dec2_input.txt', 'r') as file:
 
 def calculate_output(input, noun, verb):
     mod_input = [input[0]] + [noun] + [verb] + input[3:]
-    i = 0
-    while True:
+    for i in range (0, len(input), 4):
         match mod_input[i]:
             case 1:
                 mod_input[mod_input[i + 3]] = mod_input[mod_input[i + 1]] + mod_input[mod_input[i + 2]]
             case 2:
                 mod_input[mod_input[i + 3]] = mod_input[mod_input[i + 1]] * mod_input[mod_input[i + 2]]
             case 99:
-                break
-        i += 4
-    return mod_input[0]
+                return mod_input[0]
 
 # part 1
 print(calculate_output(input, 12, 2))
@@ -23,4 +20,3 @@ for noun in range(100):
     for verb in range(100):
         if calculate_output(input, noun, verb) == 19690720:
             print(100 * noun + verb)
-            exit()
