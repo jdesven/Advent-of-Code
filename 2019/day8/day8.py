42ed111e01a3cd3206ec1398ab1389c423b7ed8b
jdesven <mrclonetrooper2@gmail.com>
2025-01-23 16:53:08 +0100

    write-up

--- 2019/day8/day8.py
+++ 2019/day8/day8.py
@@ -1,14 +1,12 @@
 from pyhelper.pyimport import seperator_to_list
 input = seperator_to_list('2019/input/day8_input.txt', cast = int)
 
-# part 1
 img_size = [25, 6]
 nums_per_layer = [input[i:i + (img_size[0] * img_size[1])] for i in range(0, len(input), (img_size[0] * img_size[1]))]
 zeros_per_layer = [layer.count(0) for layer in nums_per_layer]
 layer_least_zeros = zeros_per_layer.index(min(zeros_per_layer))
 print(nums_per_layer[layer_least_zeros].count(1) * nums_per_layer[layer_least_zeros].count(2))
 
-# part 2
 img = [2] * (img_size[0] * img_size[1])
 for layer in range(len(nums_per_layer)):
     for i_num, num in enumerate(nums_per_layer[layer]):
